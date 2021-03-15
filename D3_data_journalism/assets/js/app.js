ed783ae4232de5368da4132035d5520a97357f0d
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-14 20:13:21 -0500

    Set up Chart Dimensions

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+// Goal: Use the D3 Library to create a scatter plot examining the relationship between poverty level and healthcare
+
+// Set up Chart
+let svgWidth = 960;
+let svgHeight = 500;
+
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+};
+
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
+
